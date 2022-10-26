4a69ab242bf4371d5f9baad9da5b726120f544b7
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-10-26 15:20:47 +0300

    separate app.js and index.js. Finish 4.2

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-// utils/config
-const config = require('./utils/config')
-// models/blog
-const Blog = require('./models/blog')
-// controllers/blogs
-const blogsRouter = require('./controllers/blogs')
-
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
