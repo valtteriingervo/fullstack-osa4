cf6fff3cb51ce95febba22311f585b63edacd409
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-10-26 15:12:47 +0300

    working controllers

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const config = require('./utils/config')
 // models/blog
 const Blog = require('./models/blog')
+// controllers/blogs
+const blogsRouter = require('./controllers/blogs')
 
 const http = require('http')
 const express = require('express')
@@ -15,25 +17,10 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+const server = http.createServer(app)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
