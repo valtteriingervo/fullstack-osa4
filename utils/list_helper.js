249e517fa26944a0ae8bb4253b77d1b4892206a8
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-10-28 14:31:41 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  else {
+    return blogs.reduce((sum, blog) => {
+      return sum + blog.likes
+    }, 0)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
 
