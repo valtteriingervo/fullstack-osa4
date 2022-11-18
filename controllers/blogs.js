6806a73ce67007f28bc2a23d9ccb8d8a7743941c
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-11-18 09:38:55 +0200

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
@@ -14,17 +17,29 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   }
   else {
-    // If likes field is not defined, give it a value of zero
+    const user = await User.findById(body.userId)
+
     const blog = body.likes
-      ? new Blog(body)
+      ? new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+      })
       : new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: 0
+        likes: 0, // If likes field is not defined, give it a value of zero
+        user: user._id
       })
 
     const savedBlog = await blog.save()
+    // the blog must be saved also to the user document
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   }
 })
